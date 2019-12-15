627c1e44fc248c537a8453e5af80528cf9b007b2
Ualter Junior <ualter.junior@gmail.com>
2019-12-15 16:57:34 +0100

    docs: updated

---
+++ sandbox/packages/app-sample/__tests__/app-sample.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const appSample = require('..');
+
+describe('app-sample', () => {
+    it('needs tests');
+});
---
+++ sandbox/packages/app-sample/lib/app-sample.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = appSample;
+
+function appSample() {
+    // TODO
+}
